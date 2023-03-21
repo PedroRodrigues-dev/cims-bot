9d7c81969c9784061edaabce9e6f00d311fb892d
Pedro Rodrigues <pedroras004@gmail.com>
2023-03-20 21:26:36 -0300

    DCB-0003 Remove token do codigo

--- configs/environment.py
+++ configs/environment.py
@@ -2,7 +2,4 @@ import os
 
 
 def discordToken():
-    return (
-        os.getenv("CIMS_BOT_DISCORD_TOKEN")
-        or "MTA4NzUyOTkzNzEzMDUwNDI3Mg.G7OYZP.rQKkgA6OaJcue9G516_QOeXw4UL6azOUfxgtyk"
-    )
+    return os.getenv("CIMS_BOT_DISCORD_TOKEN")
